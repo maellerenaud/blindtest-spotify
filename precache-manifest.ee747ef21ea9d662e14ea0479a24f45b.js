6c3875176ee9e93101342f9b01132b5fac93cfa8
MaelleRenaud <maelle.renaud@student-cs.fr>
2020-02-25 20:34:34 +0100

    Updates

---
+++ precache-manifest.ee747ef21ea9d662e14ea0479a24f45b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebbe679cc422f42daa8a77366a341da3",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "53e49050718c96fddc40",
+    "url": "/blindtest-spotify/static/css/main.7c714b3b.chunk.css"
+  },
+  {
+    "revision": "83df30a72c05267dec17",
+    "url": "/blindtest-spotify/static/js/2.1e8fa736.chunk.js"
+  },
+  {
+    "revision": "2335ae6d6f26f20df7c21ac3adc54dd1",
+    "url": "/blindtest-spotify/static/js/2.1e8fa736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53e49050718c96fddc40",
+    "url": "/blindtest-spotify/static/js/main.9f49f7fa.chunk.js"
+  },
+  {
+    "revision": "b2ffae4b889754784376",
+    "url": "/blindtest-spotify/static/js/runtime-main.bce9f056.js"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  }
+]);
