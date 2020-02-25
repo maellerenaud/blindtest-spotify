474696191e28421b8cad75e4a21182f58f7b899b
MaelleRenaud <maelle.renaud@student-cs.fr>
2020-02-25 21:25:59 +0100

    Backlog 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+const apiToken = 'BQDFDtMglEcDl2bUx3roEJh4Tg1syDAur17nndc1S5iCuPmXc5tMYkt3MlVHg8KYnMpjBywemr2I8yAIqaJINks_zkmaUsifEnnb52N7bfHgNikmVUkhYs1srBfEjafXAaKzPg30cN_cBvyPhToV0JgYm3M-NQMAIt-8i9eatrih';
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -32,22 +32,53 @@ class App extends Component {
 
   constructor() {
     super();
+    this.state = {
+      text: "",
+      data: [],
+      songsLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ text: "Bonjour !" });
+    fetch('https://api.spotify.com/v1/playlists/1wCB2uVwBCIbJA9rar5B77/tracks', {
+      method: 'GET',
+      headers: {
+      Authorization: 'Bearer ' + apiToken,
+      },
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log("Réponse reçue ! Voilà les musiques reçues : ", data);
+        this.setState({ data: data.items });
+        console.log(this.state.data);
+        this.setState({ songsLoaded: true });
+        this.setState({ text: "J'ai reçu " + this.state.data.length + " musiques. La première est " + this.state.data[0].track.name +"." })
+      })
+      
+    ;
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Bienvenue sur le Blindtest</h1>
-        </header>
-        <div className="App-images">
-          <p>Il va falloir modifier le code pour faire un vrai blindtest !</p>
+    if (!this.state.songsLoaded) {
+      return (
+        <img src={logo} className="App-logo" alt="logo"/>
+      )
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h1 className="App-title">Bienvenue sur le Blindtest</h1>
+          </header>
+          <div className="App-images">
+            <p>{this.state.text}</p>
+          </div>
+          <div className="App-buttons">
+          </div>
         </div>
-        <div className="App-buttons">
-        </div>
-      </div>
-    );
+      )
+    };
   }
 }
 
